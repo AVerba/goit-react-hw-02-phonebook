ae0f8960bd1d84fbe05376a4bb3c364ef9b8aea0
Oleksii Verbytskyi <alverba88@gmail.com>
2022-05-07 22:01:15 +0300

    added Input/Filter comp

--- src/components/ContactFilter/ContactFilter.jsx
+++ src/components/ContactFilter/ContactFilter.jsx
@@ -1,1 +1,18 @@
 import styles from './ContactFilter.module.css';
+import { Container } from '../Container';
+import { Title } from '../ui/Title';
+import { Input } from '../ui/Input';
+
+export const ContactFilter = ({ value, onChange }) => {
+  return (
+    <Container>
+      <Title>Find contacts by name</Title>
+      <Input
+        type="text"
+        value={value}
+        onChange={onChange}
+        placeholder="Search name"
+      />
+    </Container>
+  );
+};
--- src/components/ui/Input/Input.jsx
+++ src/components/ui/Input/Input.jsx
@@ -1,1 +1,30 @@
 import styles from './Input.module.css';
+import { Title } from '../Title';
+import { Container } from '../../Container';
+
+export const Input = ({
+  title,
+  type,
+  name,
+  value,
+  placeholder,
+  pattern,
+  onChange,
+}) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <input
+        type={type}
+        name={name}
+        key={name}
+        value={value}
+        onChange={onChange}
+        pattern={pattern}
+        placeholder={placeholder}
+        autoComplete="off"
+        required
+      />
+    </Container>
+  );
+};
