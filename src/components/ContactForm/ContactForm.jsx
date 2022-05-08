66751baa1473357a1455c0d7c3541a0b974f5de2
Oleksii Verbytskyi <alverba88@gmail.com>
2022-05-08 16:16:50 +0300

    finished PhoneBook task

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -57,7 +57,7 @@ export default class ContactForm extends Component {
           <input
             type="text"
             name="name"
-            placeholder="john doe"
+            placeholder="John John"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             value={this.state.name}
